c1677714030661c2aabfa11b1d6dd1c9418c71d0
Nexoreon <tiger000226@gmail.com>
2023-09-28 17:46:06 +0300

    add route for sendStatus

--- routes/systemRoutes.ts
+++ routes/systemRoutes.ts
@@ -1,15 +1,17 @@
 import { Router } from 'express';
 import { getSettings, createSettings, updateSettings } from '../controllers/settingsController';
 import { getNotifications } from '../controllers/notificationController';
+import { sendStatus } from '../controllers/appController';
 
 const router = Router();
 
+router.get('/', sendStatus);
 // TODO: add route protection
-router.route('/settings')
+router.route('/api/v1/settings')
 .get(getSettings)
 .post(createSettings)
 .patch(updateSettings);
 
-router.get('/notifications', getNotifications);
+router.get('/api/v1/notifications', getNotifications);
 
 export default router;
